f4166c64a23f6fe22a700d87d8d8cc5682408f60
TwoSpikes <105787986+TwoSpikes@users.noreply.github.com>
2022-10-05 09:02:44 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-if [[ $0 == '' ]];
-    set outfile 'main.c.out';
-else
-    set outfile $0;
-export outfile;
-
-clang --std=c2x main.c -o $outfile;
+clang --std=c++17 main.cpp -o main.cpp.out;
