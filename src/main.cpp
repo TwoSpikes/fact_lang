8df12b990102a9f9f1fc73fe9206d278b8b89e34
TwoSpikes <twospikesfpg@gmail.com>
2022-10-11 22:22:05 +0300

    Delete cmake files and refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
-#include <cstdio>
+#include <iostream>
 #include <string>
 #include <vector>
 
 #include "./globals/rust_types.hpp"
-#include "./parser/parse_argv.hpp"
+
+#include "./lexer/parse_argv.hpp"
 
 int main(int argc, char** argv) {
-  std::vector<std::string> tmp = parse_argv(argc, argv);
-  for(std::string i : tmp) {
-    printf("%s", i.c_str());
+  auto tmp = new std::vector<std::string>(parse_argv(argc, argv));
+  for(auto i = tmp->begin(); i != tmp->end(); i++) {
+    std::cout << i->c_str();
   }
   return 0;
 }
