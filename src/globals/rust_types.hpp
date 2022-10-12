97afff3310aa40bd1c147bbb28b6aa252ab9aedf
TwoSpikes <twospikesfpg@gmail.com>
2022-10-12 17:34:07 +0300

    Update src/globals/rust_types.hpp

--- src/globals/rust_types.hpp
+++ src/globals/rust_types.hpp
@@ -9,3 +9,14 @@ typedef u_int8_t  u8;
 typedef u_int16_t u16;
 typedef u_int32_t u32;
 typedef u_int64_t u64;
+
+
+typedef i8  I1;
+typedef i16 I2;
+typedef i32 I4;
+typedef i64 I8;
+
+typedef u8  U1;
+typedef u16 U2;
+typedef u32 U4;
+typedef u64 U8;
